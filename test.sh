de6e8f8cc21378d6967bc71d45a263262bf995f4
isro01 <ishir@iitk.ac.in>
2020-09-08 22:04:14 +0530

    checking for test error

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 for cf in *; do
-    cpplint --filter=-legal/copyright,-readability/multiline_comment,-readability/braces,-build/include_order,-build/c++11,-build/include_what_you_use,-runtime/string,-whitespace/indent,+build/namespace,+readability/constructors --linelength=160 $cf
+  cpplint --filter=-whitespace,-legal/copyright,-readability/multiline_comment, --linelength=160 $cf
 
 done
 
